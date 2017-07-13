844e7546c31a9ac89ae79cfcbaadcec798dd5491
avdeevalexsey <avdeevalexsey@gmail.com>
2017-07-13 16:16:46 +0700

    13.07.17

--- WoocomerceAdvanceWP.php
+++ WoocomerceAdvanceWP.php
@@ -13,31 +13,43 @@ add_action('add_meta_boxes', 'WoocomerceAdvanceWP_add_custom_box');
 
 /* HTML код блока */
 function WoocomerceAdvanceWPShowMetaBox($post) {
-	echo '<script type="text/javascript" src="'.plugins_url('/WoocomerceAdvanceWP/script.js').'"></script>';
-	// Используем nonce для верификации
-	//wp_nonce_field( plugin_basename(__FILE__), 'myplugin_noncename' );
-	echo "<div id='WoocomerceAdvanceWPShowMetaBox'>";
-	// Поля формы для введения данных
-	echo '<label for="myplugin_new_field">' . __("Description for this field", 'myplugin_textdomain' ) . '</label> ';
 
 	$data  = unserialize(get_post_meta($post->ID,'_my_meta_value_key',true));
-
 	print_r($data);
-	echo "string";
-	echo '<input type="text" id= "shop" name="shop" value="'.get_post_meta($post->ID,'_my_meta_value_key',true).'" size="25" />';
-	echo '<input type="text" id= "myplugin_new_field" name="myplugin_new_field" value="'.get_post_meta($post->ID,'_my_meta_value_key',true).'" size="25" />';
 
-	echo "<button onclick='add_market_meta(); return false;'>ADD</button>";
+
+	echo "<div id='WoocomerceAdvanceWPShowMetaBox'>";
+	echo "";
+	$i = 0;
+	foreach ($data as $key => $market) {
+		echo "<div id='MarkeID".$i."'>";
+		echo 'MarketName <input type="text" 	name="MarketName_N'.$i.'" value="'.$market['MarketName'].'"><br>';
+		echo 'ProdName 	 <input type="text" 	name="ProdName_N'.$i.'"   value="'.$market['ProdName'].'"><br>';
+		echo 'ProdPrice  <input type="number" 	name="ProdPrice_N'.$i.'"  value="'.$market['ProdPrice'].'"><br>';
+		echo 'ProdWeight <input type="number" 	name="ProdWeight_N'.$i.'" value="'.$market['ProdWeight'].'"><br>';
+		echo 'ProdInfo 	 <input type="text" 	name="ProdInfo_N'.$i.'"   value="'.$market['ProdInfo'].'"><br>';
+		echo 'InStock 	 <input type="checkbox"	name="InStock_N'.$i.'"	  value="'.$market['InStock'].'"><br>';
+		echo "<input type='button' onclick='remove_market_meta(\"MarkeID".$i."\")' value='Remove'>";
+		echo '<hr>';
+		echo "</div>";
+		$i++;	
+	}
+
+	echo "<script>var i =".$i.";</script>";
+
+	
 	echo "</div>";
-}
-?>
 
-<?php 
+	echo '<script type="text/javascript" src="'.plugins_url('/WoocomerceAdvanceWP/script.js').'"></script>';
+
+	
+	echo '<input type="hidden" name="count_markets" id="count_markets" value="'.$i.'">';
+	echo "<input type='button' onclick='add_market_meta(i)' value='ADD'>";
+
+}
+
 /* Сохраняем данные, когда пост сохраняется */
 function WoocomerceAdvanceWPSavePostdata( $post_id ) {
-	// проверяем nonce нашей страницы, потому что save_post может быть вызван с другого места.
-	//if ( ! wp_verify_nonce( $_POST['myplugin_noncename'], plugin_basename(__FILE__) ) )
-	//	return $post_id;
 
 	// проверяем, если это автосохранение ничего не делаем с данными нашей формы.
 	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
@@ -50,14 +62,22 @@ function WoocomerceAdvanceWPSavePostdata( $post_id ) {
 		return $post_id;
 	}
 
+	$counter =   sanitize_text_field( $_POST['count_markets'] );
+
 	// Убедимся что поле установлено.
-	if ( ! isset( $_POST['myplugin_new_field'] ) )
-		return;
+	if ( $counter == 0)	return;
 
 	// Все ОК. Теперь, нужно найти и сохранить данные
-	// Очищаем значение поля input.
-	$my_data[] = sanitize_text_field( $_POST['shop'] );
-	$my_data[] = sanitize_text_field( $_POST['myplugin_new_field'] );
+
+	for ($i=0; $i < $counter; $i++) { 
+		$my_data[$i]['MarketName'] 	= sanitize_text_field( $_POST['MarketName_N'.$i] );
+		$my_data[$i]['ProdName'] 	= sanitize_text_field( $_POST['ProdName_N'.$i] );
+		$my_data[$i]['ProdPrice'] 	= sanitize_text_field( $_POST['ProdPrice_N'.$i] );
+		$my_data[$i]['ProdWeight'] 	= sanitize_text_field( $_POST['ProdWeight_N'.$i] );
+		$my_data[$i]['ProdInfo'] 	= sanitize_text_field( $_POST['ProdInfo_N'.$i] );
+		$my_data[$i]['InStock'] 	= sanitize_text_field( $_POST['InStock_N'.$i] );
+
+	}
 
 
 	$data = serialize($my_data);
@@ -66,6 +86,5 @@ function WoocomerceAdvanceWPSavePostdata( $post_id ) {
 }
 add_action( 'save_post', 'WoocomerceAdvanceWPSavePostdata' );
 
-//if (is_admin()) echo "<script>alert('ADMIN');</script>";
 
  ?>
--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+
 function add_market_meta(){
-	meta_block = document.getElementById('WoocomerceAdvanceWPShowMetaBox');
-	var form = 'test:<input type="text"><br/>'
-	meta_block.innerHTML =  meta_block.innerHTML+form;
+    var meta_block = document.getElementById('WoocomerceAdvanceWPShowMetaBox'), fragment, input, div;
+    
+    fragment = document.createElement("div");
+    fragment.id = "MarkeID"+i;
+    fragment.appendChild(document.createTextNode("MarketName "));
+    input = document.createElement("input");
+    input.type = "text";
+    input.name = "MarketName_N" + i;
+
+
+    fragment.appendChild(input);    
+    fragment.appendChild(document.createElement("br"));
+    
+    fragment.appendChild(document.createTextNode("ProdName "));
+    input = document.createElement("input");
+    input.type = "text";
+    input.name = "ProdName_N" + i;
+    fragment.appendChild(input);
+    fragment.appendChild(document.createElement("br"));
+    
+
+	input = document.createElement("input");
+	input.type = "button";
+	input.value = "Remove";
+	input.setAttribute("onclick", "remove_market_meta('MarkeID"+ i +"')");
+
+	fragment.appendChild(input);
+    fragment.appendChild(document.createElement("hr"));
+    i++;
+    document.getElementById('count_markets').value = i;
+    meta_block.appendChild(fragment);
+};
+
+
+function remove_market_meta(_id){
+	var elem = document.getElementById(_id);
+	elem.parentNode.removeChild(elem);
 }
+
